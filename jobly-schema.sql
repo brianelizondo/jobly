31fa43a80ce28dba341a8909537963c892718055
Brian Elizondo <brian_elizondo@msn.com>
2022-07-14 20:23:21 -0400

    Express Jobly => Further Study: Use enum Type - Change the state column in the applications table to be an 'enum type' that consists of 'interested', 'applied', 'accepted', 'rejected'

--- jobly-schema.sql
+++ jobly-schema.sql
@@ -25,10 +25,13 @@ CREATE TABLE jobs (
     REFERENCES companies ON DELETE CASCADE
 );
 
+CREATE TYPE job_state AS ENUM ('interested', 'applied', 'accepted', 'rejected');
+
 CREATE TABLE applications (
   username VARCHAR(25)
     REFERENCES users ON DELETE CASCADE,
   job_id INTEGER
     REFERENCES jobs ON DELETE CASCADE,
+  state job_state DEFAULT NULL,
   PRIMARY KEY (username, job_id)
 );
